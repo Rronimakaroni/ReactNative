67ec9a2f63e12fd05a57625db0a9792c7fb0bd18
RronAbdullahu <rronabdullahu01@gmail.com>
2024-11-12 21:00:50 +0100

    Firstactualproject

---
+++ Module3/first-react-project/screens/MainScreen.js
@@ -0,0 +1,9 @@
+import React from "react";
+import{Text,StyleSheet, View} from "react-native";
+const MainScreen =()=>{
+    return <View>
+        <Text>This is from Main Screen</Text>
+    </View>
+}
+
+export default MainScreen;
